ab3c5c1dd6b0ed8c7005f744d645990101861841
yogenns <yogenns@gmail.com>
2020-08-18 23:02:00 +0530

    Task 7 - View Flash Card (Toggle On Click)

--- app/src/main/java/tool/yogendra/flashcard/adapter/FlashCardRecyclerAdapter.java
+++ app/src/main/java/tool/yogendra/flashcard/adapter/FlashCardRecyclerAdapter.java
@@ -1,9 +1,11 @@
 package tool.yogendra.flashcard.adapter;
 
 import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -29,16 +31,41 @@ public class FlashCardRecyclerAdapter extends RecyclerView.Adapter<FlashCardRecy
         return new FlashCardRecyclerAdapter.ViewHolder(v);
     }
 
+    int selectedPosition = -1;
+
     @Override
-    public void onBindViewHolder(@NonNull FlashCardRecyclerAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final FlashCardRecyclerAdapter.ViewHolder holder, final int position) {
         final FlashCardParams item = listFlashCards.get(position);
         holder.flashCardFront.setText(item.getFlashCardFront());
         holder.flashCardBack.setText(item.getFlashCardBack());
         if (item.getFlashCardSectionColor() != null && !item.getFlashCardSectionColor().isEmpty()) {
             holder.flashCardSectionColor.setBackgroundColor(Color.parseColor(item.getFlashCardSectionColor()));
         } else {
-            holder.flashCardSectionColor.setBackgroundColor(Color.parseColor("#FF424242"));
+            holder.flashCardSectionColor.setBackgroundColor(Color.parseColor("#FF424242")); // Gray
         }
+        if (selectedPosition == position) {
+            holder.flashCardFrontLayout.setVisibility(View.INVISIBLE);
+            holder.flashCardBackLayout.setVisibility(View.VISIBLE);
+            holder.flashCardView.setCardBackgroundColor(Color.parseColor("#FC0434")); // Red
+        } else {
+            holder.flashCardFrontLayout.setVisibility(View.VISIBLE);
+            holder.flashCardBackLayout.setVisibility(View.INVISIBLE);
+            holder.flashCardView.setCardBackgroundColor(Color.parseColor("#FF424242")); // Gray
+        }
+        holder.flashCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("OnClick", "Front Visibility " + holder.flashCardFrontLayout.getVisibility());
+                Log.i("OnClick", "Back Visibility " + holder.flashCardBackLayout.getVisibility());
+                if (selectedPosition != position) {
+                    selectedPosition = position;
+                } else {
+                    selectedPosition = -1;
+                }
+                notifyDataSetChanged();
+                //Log.i("OnClick","");
+            }
+        });
     }
 
     @Override
@@ -51,6 +78,8 @@ public class FlashCardRecyclerAdapter extends RecyclerView.Adapter<FlashCardRecy
         TextView flashCardFront;
         TextView flashCardSectionColor;
         TextView flashCardBack;
+        LinearLayout flashCardFrontLayout;
+        LinearLayout flashCardBackLayout;
 
         ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -58,6 +87,8 @@ public class FlashCardRecyclerAdapter extends RecyclerView.Adapter<FlashCardRecy
             flashCardFront = itemView.findViewById(R.id.flashCardFrontTxt);
             flashCardSectionColor = itemView.findViewById(R.id.flashCardSectionColorTxt);
             flashCardBack = itemView.findViewById(R.id.flashCardBackTxt);
+            flashCardFrontLayout = itemView.findViewById(R.id.frontLayout);
+            flashCardBackLayout = itemView.findViewById(R.id.backLayout);
         }
     }
 }
